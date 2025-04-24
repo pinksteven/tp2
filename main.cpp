ca34e70dcd0bc73261b65196c661a3856e6f981c
Steven <st.latuszek@gmail.com>
2025-04-24 18:29:59 +0200

    comments

--- main.cpp
+++ main.cpp
@@ -6,11 +6,15 @@
 #include <vector>
 
 std::tuple<std::queue<int>, std::queue<int>> deal_cards() {
+  // Represent cards as numbers will make value comparing easier
   std::vector<int> deck{2,  2,  2,  2,  3,  3,  3,  3,  4,  4,  4,  4,  5,
                         5,  5,  5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,
                         8,  8,  9,  9,  9,  9,  10, 10, 10, 10, 11, 11, 11,
                         11, 12, 12, 12, 12, 13, 13, 13, 13, 14, 14, 14, 14};
+  // easiest way to shuffle the deck i think. mersenne_twister_engine bc it's
+  // used as expamle on cppreference
   std::shuffle(deck.begin(), deck.end(), std::mt19937(std::random_device()()));
+  // well i gotta push one by one anyway (i think) so we going back and forth
   std::queue<int> player1, player2;
   for (int i = 0; i < 52; i++) {
     if (i % 2 == 0)
@@ -18,6 +22,7 @@ std::tuple<std::queue<int>, std::queue<int>> deal_cards() {
     else
       player2.push(deck[i]);
   }
+  // easier to return one thing so we tuple both hands
   return std::make_tuple(player1, player2);
 }
 
